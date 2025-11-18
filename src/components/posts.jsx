d093562867cddf6d626c905eb11fff99496e3af0
Swift-Lore <swiftlore13@gmail.com>
2025-11-18 01:38:21 -0800

    Updated posts.jsx

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,14 +1,6 @@
 // src/components/Posts.jsx
-import Header_2 from './header_2';
-import TimelineBody from './timeline-body';
-import Footer from './footer';
+import TimelineBody from "./timeline-body";
 
 export default function Posts() {
-    return (
-    <>
-      <Header_2 />
-      <TimelineBody />
-      <Footer />
-    </>
-    );
+  return <TimelineBody />;
 }
