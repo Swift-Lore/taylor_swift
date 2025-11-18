1e967f22ba8d56daf26ed01bd688be5ea9d66d48
Swift-Lore <swiftlore13@gmail.com>
2025-11-18 01:49:07 -0800

    Updated posts.jsx

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,6 @@
-// src/components/Posts.jsx
-import TimelineBody from "./timeline-body";
+// src/components/post_details.jsx
+import Post_detail_body from "./post_detail_body.jsx";
 
-export default function Posts() {
-  return <TimelineBody />;
+export default function Post_detail() {
+  return <Post_detail_body />;
 }
