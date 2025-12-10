d6cc30ee240151947ea654f35023130c6800a9aa
Swift-Lore <swiftlore13@gmail.com>
2025-12-10 13:16:29 -0800

    Updated ErasTourShows.jsx

--- src/components/ErasTourShows.jsx
+++ src/components/ErasTourShows.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import Select from "react-select";
+import AdComponent from "./ad_component";
 
 const SERVER_EVENTS_ENDPOINT = import.meta.env.VITE_EVENTS_ENDPOINT || "";
 
