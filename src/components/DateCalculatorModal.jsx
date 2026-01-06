8ecafc765023169fc8d1054afbfa5681f837e6f7
Swift-Lore <swiftlore13@gmail.com>
2026-01-06 15:54:53 -0800

    Updated DateCalculatorModal.jsx

--- src/components/DateCalculatorModal.jsx
+++ src/components/DateCalculatorModal.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button } from "./ui/Button"
 
 export default function DateCalculatorModal({ onClose }) {
