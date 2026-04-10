430eaac5ebbf0c85102c3ff6e3c259c717eb0a2b
Swift-Lore <swiftlore13@gmail.com>
2026-04-09 18:53:11 -0700

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-import { HelmetProvider } from "react-helmet-async" // 1. Import the provider
+import { HelmetProvider } from "react-helmet-async"
 import App from "./App.jsx"
 import "./index.css"
 
