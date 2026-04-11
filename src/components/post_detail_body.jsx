55790f8954dd039cbc0bed3a0a124cc14017916c
Swift-Lore <swiftlore13@gmail.com>
2026-04-11 15:16:01 -0700

    Updated post_detail_body.jsx

--- src/components/post_detail_body.jsx
+++ src/components/post_detail_body.jsx
@@ -1216,7 +1216,7 @@ if (isFacebook) {
                         border: "1px solid #dbdbdb",
                         boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
                         margin: "0",
-                        width: "326px",
+                        width: "360px",
                         padding: "0",
                         display: "block"
                       }}
