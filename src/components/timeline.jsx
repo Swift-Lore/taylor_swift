c540bda4608c14988ac1017650cdc8db8afd34c8
Swift-Lore <swiftlore13@gmail.com>
2026-04-12 01:15:11 -0700

    Updated timeline.jsx

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1304,7 +1304,7 @@ const hasGlobalHoliday = globalHolidayTagsForDay.length > 0
         </div>
           </div>{/* end mobileView today wrapper */}
           {/* Desktop sidebar */}
-<div className="hidden lg:block w-[320px] shrink-0 pt-[160px]">
+<div className="hidden lg:block w-[320px] shrink-0 pt-[150px]">
             <RecentEvents />
           </div>
         </div>
