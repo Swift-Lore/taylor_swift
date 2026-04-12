1a1c544fca89a0b5d7ceb20e59e18131f16ac9a2
Swift-Lore <swiftlore13@gmail.com>
2026-04-12 01:20:36 -0700

    Updated DateToolsModal.jsx

--- src/components/DateToolsModal.jsx
+++ src/components/DateToolsModal.jsx
@@ -390,7 +390,7 @@ const betweenRes = calculatorTab === "between" ? calcBetween() : null;
             </button>
           </div>
 
-<div className="mb-4 min-h-[400px]">
+<div className="flex flex-col min-h-[400px]">
             {tab === "calendar" && (
   <>
     <div className="flex gap-2 mb-4">
