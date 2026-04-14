3a88ec0a363cef33670247f607aa71f8103fd92c
Swift-Lore <swiftlore13@gmail.com>
2026-04-14 16:58:08 -0700

    Updated og-event.js

--- api/og-event.js
+++ api/og-event.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
       ? 'https://swift-lore.com/images/swift_lore.png'
       : 'https://swift-lore.com/images/star.png';
     const url = `https://swift-lore.com/post_details?id=${id}`;
-
+// Added bluesky code
     
     const html = `<!DOCTYPE html>
 <html>
